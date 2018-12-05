49a48f9e721c356fe9f19b167b9ca273fd20ff90
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2018-12-05 14:30:59 -0800

    Bump minor version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if not os.path.isfile(frotz_c_lib):
     sys.exit(1)
 
 setup(name='jericho',
-      version='1.1.1',
+      version='1.1.2',
       install_requires=[],
       description='A python interface to text-based adventure games.',
       author='Matthew Hausknecht',
