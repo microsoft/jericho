7b04b1ce0f0d3d36b12ab20e852a07379f9d1892
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2018-12-06 17:53:43 -0500

    Adding more tests

--- tests/test_textworld_games.py
+++ tests/test_textworld_games.py
@@ -5,6 +5,8 @@ import jericho
 
 
 TEST_GAME = os.path.abspath(pjoin(__file__, '..', "tw-game.z8"))
+TEST_GAME2 = os.path.abspath(pjoin(__file__, '..', "tw-game-very-long-filename-but-not-that-long-though.z8"))
+
 
 
 def test_loading_a_textworld_game():
@@ -34,3 +36,94 @@ def test_loading_a_textworld_game():
     assert done
     assert env.game_over()  # Lost
     assert score == 0
+
+
+def test_loading_several_textworld_games():
+    for i in range(10):
+        env = jericho.FrotzEnv(TEST_GAME)
+        state = env.reset()
+
+        assert not env.victory()
+        assert not env.game_over()
+        assert env.get_score() == 0
+        assert env.get_max_score() == 3
+
+        state, score, done, _ = env.step("go east")
+        assert not done
+        assert score == 0
+
+        state, score, done, _ = env.step("insert carrot into chest")
+        assert not done
+        assert score == 2
+
+        state, score, done, _ = env.step("close chest")
+        assert done
+        assert env.victory()
+        assert score == 3
+
+        state = env.reset()
+        state, score, done, _ = env.step("eat carrot")
+        assert done
+        assert env.game_over()  # Lost
+        assert score == 0
+        env.close()
+
+
+def test_loading_several_textworld_games2():
+    for i in range(10):
+        env = jericho.FrotzEnv(TEST_GAME)
+        state = env.reset()
+
+        assert not env.victory()
+        assert not env.game_over()
+        assert env.get_score() == 0
+        assert env.get_max_score() == 3
+
+        state, score, done, _ = env.step("go east")
+        assert not done
+        assert score == 0
+
+        state, score, done, _ = env.step("insert carrot into chest")
+        assert not done
+        assert score == 2
+
+        state, score, done, _ = env.step("close chest")
+        assert done
+        assert env.victory()
+        assert score == 3
+
+        state = env.reset()
+        state, score, done, _ = env.step("eat carrot")
+        assert done
+        assert env.game_over()  # Lost
+        assert score == 0
+        env.close()
+
+        env = jericho.FrotzEnv(TEST_GAME2)
+        state = env.reset()
+
+        assert not env.victory()
+        assert not env.game_over()
+        assert env.get_score() == 0
+        assert env.get_max_score() == 3
+
+        state, score, done, _ = env.step("go east")
+        assert not done
+        assert score == 0
+
+        state, score, done, _ = env.step("insert carrot into chest")
+        assert not done
+        assert score == 2
+
+        state, score, done, _ = env.step("close chest")
+        assert done
+        assert env.victory()
+        assert score == 3
+
+        state = env.reset()
+        state, score, done, _ = env.step("eat carrot")
+        assert done
+        assert env.game_over()  # Lost
+        assert score == 0
+        env.close()
+
