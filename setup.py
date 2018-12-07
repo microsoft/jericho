7753e95a6b1689dc45feb89874554381ea5d534f
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2018-12-06 19:42:13 -0500

    Add requirements file

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if not os.path.isfile(frotz_c_lib):
 
 setup(name='jericho',
       version='1.1.3',
-      install_requires=[],
+      install_requires=open('requirements.txt').readlines(),
       description='A python interface to text-based adventure games.',
       author='Matthew Hausknecht',
       packages=['jericho'],
