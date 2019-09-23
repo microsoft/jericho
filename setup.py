b2a40d06ee0f09e942802ced94bcda5a55bab237
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2019-09-23 10:03:33 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if not os.path.isfile(frotz_c_lib):
     sys.exit(1)
 
 setup(name='jericho',
-      version='1.3.0',
+      version='1.3.1',
       install_requires=['numpy'],
       description='A python interface to text-based adventure games.',
       author='Matthew Hausknecht',
