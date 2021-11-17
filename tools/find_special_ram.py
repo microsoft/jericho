b53890318b2d6b0743d77bf384a306125ef79645
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2021-11-17 16:15:26 -0500

    Refactor find_special_ram

--- tools/find_special_ram.py
+++ tools/find_special_ram.py
@@ -12,22 +12,17 @@ import jericho
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("filenames", nargs="+",
-                        help="Path to a Z-Machine game(s).")
+    parser.add_argument("filename",
+                        help="Path to a Z-Machine game.")
     parser.add_argument("--index", type=int, help="Index of the walkthrough command to investigate.")
-    parser.add_argument("--interactive", action="store_true",
-                        help="Type the command.")
-    parser.add_argument("--debug", action="store_true",
-                        help="Launch ipdb on FAIL.")
+    parser.add_argument("--alternative-command",
+                        help="Type the command to replace the one at '--index'.")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="Print the last observation when not achieving max score.")
     parser.add_argument("-vv", "--very-verbose", action="store_true",
                         help="Print the last observation when not achieving max score.")
-    parser.add_argument("--check-state", action="store_true",
-                        help="Check if each command changes the state.")
     return parser.parse_args()
 
-args = parse_args()
 
 
 def get_zmp(env):
@@ -128,32 +123,19 @@ def show_mem_diff(M0, M1, M2, start=24985, length=240*2):
         print(f"{i:3d} [{start+i*2:5d}]: {M1[i]:5d} -> {M2[i]:5d}")
 
 
-filename_max_length = max(map(len, args.filenames))
-for filename in sorted(args.filenames):
-    rom = os.path.basename(filename)
-    print(filename.ljust(filename_max_length))#, end=" ")
-
-    env = jericho.FrotzEnv(filename)
-    if not env.is_fully_supported:
-        print(colored("SKIP\tUnsupported game", 'yellow'))
-        continue
-
-    if "walkthrough" not in env.bindings:
-        print(colored("SKIP\tMissing walkthrough", 'yellow'))
-        continue
-
+def collect_zmp(env, walkthrough, verbose=False):
     env.reset()
     Z = get_zmp(env)
 
     history = []
-    history.append((0, 'reset', env.get_state(), Z))
+    # history.append((0, 'reset', env.get_state(), Z))
+    history.append({"cmd": 'reset', "state": env.get_state(), "zmp": Z})
 
-    walkthrough = env.get_walkthrough()
     cmd_no = 0
 
-    noise_indices = set()
-    relevant_indices = set()
-    changes_history = []
+    # noise_indices = set()
+    # relevant_indices = set()
+    # changes_history = []
     cpt = 0
     while True:
         if cmd_no >= len(walkthrough):
@@ -161,84 +143,87 @@ for filename in sorted(args.filenames):
 
         cmd = walkthrough[cmd_no].lower()
 
-        if args.interactive:
-            manual_cmd = input(f"{cpt}. [{cmd}]> ")
-            if manual_cmd.strip():
-                cmd = manual_cmd.lower()
-        else:
+        if verbose:
             print(f"{cpt}. > {cmd}")
 
         if cmd == walkthrough[cmd_no].lower():
             cmd_no += 1
 
-        last_env_objs = env.get_world_objects(clean=False)
-        last_env_objs_cleaned = env.get_world_objects(clean=True)
-
-        last_Z = Z
-        obs, rew, done, info = env.step(cmd)
+        # last_Z = Z
+        obs, _, _, _ = env.step(cmd)
         cpt += 1
-        # print(">", cmd)
-        print(obs)
-
-        env_objs = env.get_world_objects(clean=False)
-        env_objs_cleaned = env.get_world_objects(clean=True)
+        if verbose:
+            print(obs)
 
         Z = get_zmp(env)
 
-        changes = set(np.nonzero(Z != last_Z)[0])
+        # changes = set(np.nonzero(Z != last_Z)[0])
 
-        history.append((cmd, env.get_state(), Z))
-        changes_history.append(changes)
+        history.append({'cmd': cmd, "state": env.get_state(), "zmp": Z})
+        # changes_history.append(changes)
 
-        # ans = ""
-        # while ans not in {'y', 'n', 's'}:
-        #     ans = input("State has changed? [y/n/s]> ").lower()
+    return history#, changes_history
 
-        # if ans == "y":
-        #     relevant_indices |= changes - noise_indices
-        # elif ans == "s":
-        #     pass
-        # elif ans == "n":
-        #     noise_indices |= changes
-        #     relevant_indices -= noise_indices
-        # else:
-        #     assert False
 
-        #print(special_indices)
+def compute_zmp_changes(history):
+    last_Z = history[0]['zmp']
+    changes = []
+    for state in history[1:]:
+        Z = state['zmp']
+        changes.append(set(np.nonzero(Z != last_Z)[0]))
+        last_Z = Z
 
-        # display_relevant(Z, changes, relevant_indices, noise_indices)
+    return changes
 
-        def display_objs_diff():
-            print("Objects diff:")
-            for o1, o2 in zip(last_env_objs, env_objs):
-                if o1 != o2:
-                    print(colored(f"{o1}\n{o2}", "red"))
 
-            print("Cleaned objects diff:")
-            for o1, o2 in zip(last_env_objs_cleaned, env_objs_cleaned):
-                if o1 != o2:
-                    print(colored(f"{o1}\n{o2}", "red"))
+def display_unique_changes(idx, history, changes_history):
+    counter = {idx: 0 for idx in changes_history[idx]}
+    matches = {idx: [] for idx in changes_history[idx]}
+    for i, changes in enumerate(changes_history):
+        for idx in changes:
+            if idx in counter:
+                counter[idx] += 1
+                matches[idx].append((i, history[i+1]["cmd"], history[i+1]["zmp"][idx]))
 
-        # breakpoint()
+    for idx, count in sorted(counter.items(), key=lambda e: e[::-1]):
+        if matches[idx][0][0] == 0:
+            continue
 
-    def search_unique_changes(idx):
-        counter = {idx: 0 for idx in changes_history[idx]}
-        matches = {idx: [] for idx in changes_history[idx]}
-        for i, changes in enumerate(changes_history):
-            for idx in changes:
-                if idx in counter:
-                    counter[idx] += 1
-                    matches[idx].append((i, history[i+1][0], history[i+1][2][idx]))
+        print(f"{idx:6d}: {count:3d} : " + ", ".join(f"{i}.{cmd}({value})" for i, cmd, value in matches[idx][:20]))
 
-        for idx, count in sorted(counter.items(), key=lambda e: e[::-1]):
-            # if matches[idx][0][0] == 0:
-                # continue
 
-            print(f"{idx:6d}: {count:3d} : " + ", ".join(f"{i}.{cmd}({value})" for i, cmd, value in matches[idx][:20]))
+def main():
+    args = parse_args()
 
-    #if len(changes_history) > :
-    print(f"Ram changes unique to command: {args.index}. > {history[args.index+1][0]}")
-    search_unique_changes(args.index)
-    # display_indices(search_unique_changes(args.index))
-    if args.debug:
-        breakpoint()
+    env = jericho.FrotzEnv(args.filename)
+    print(args.filename)
+    if not env.is_fully_supported:
+        print(colored("SKIP\tUnsupported game", 'yellow'))
+        return
+
+    if "walkthrough" not in env.bindings:
+        print(colored("SKIP\tMissing walkthrough", 'yellow'))
+        return
+
+    walkthrough = env.get_walkthrough()
+    history = collect_zmp(env, walkthrough, verbose=False)
+    changes_history = compute_zmp_changes(history)
+
+    print(f"Ram changes unique to command: {args.index}. > {history[args.index+1]['cmd']}")
+    if args.alternative_command:
+        walkthrough_ = walkthrough[:args.index] + [args.alternative_command] + walkthrough[args.index+1:]
+        history2 = collect_zmp(env, walkthrough_, verbose=False)
+        changes_history2 = compute_zmp_changes(history2)
+        assert len(changes_history) == len(changes_history2)
+
+        for i in range(len(changes_history)):
+            changes_history[i] = changes_history[i] & changes_history2[i]
+
+        print(f"intersected with ram changes unique to command: {args.index}. > {args.alternative_command}")
+
+
+    display_unique_changes(args.index, history, changes_history)
+
+
+if __name__ == "__main__":
+    main()
